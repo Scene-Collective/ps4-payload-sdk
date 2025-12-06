e540789664155d3bcacbe4f050c620ff1d39df51
Justus Schneider <geil.js18@gmail.com>
2025-12-06 19:25:00 +0100

    Add support for multiple Linux distributions in install.sh
    Refactor installation script to handle different Linux distributions. Additionally add the option for users to manually confirm library installation

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+!/bin/bash
 
 # DEPRECIATED: Left for example on how you *could* install it
 
@@ -7,8 +7,21 @@ if [ "$EUID" -ne 0 ]; then
   echo "Not root, skipping update and install"
   exit
 else
-  apt-get update
-  apt-get install -y --no-install-recommends binutils gcc make
+  DISTRO=$( lsb_release -i | cut -f 2- | tr '[:upper:]' '[:lower:]' )
+  if [[ $DISTRO == "ubuntu" ]] then
+    apt-get update
+    apt-get install -y --no-install-recommends binutils gcc make
+  elif [[ $DISTRO == "opensuse" ]] then
+    zypper install -y binutils gcc make
+  elif [[ $DISTRO == "fedora" ]] then
+    dnf install -y binutils binutils-gold gcc make
+  else
+    echo "OS not implemented yet, please manually install the required libraries and then continue (binutils, gcc and make)."
+    read -p "Have you installed the required libraries? " yn
+    if [ $yn != [yY] || $yn != [yY][eE][sS] ]] then
+      exit
+    fi
+  fi
 fi
 
 # Delete directory if it exists and make empty directory
